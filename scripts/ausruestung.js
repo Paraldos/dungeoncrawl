d5c83de04908b3779d1cd462e5ec7e4947617a05
Paraldos <hillbrecht.s@gmail.com>
2024-04-01 11:02:22 +0200

    Styling ausruestung

--- scripts/ausruestung.js
+++ scripts/ausruestung.js
@@ -1,15 +1,17 @@
 export default class Ausruestung {
-    constructor() {
-        this.checkboxes = [... document.querySelector('.vorratspunkte__checkboxes').children];
+  constructor() {
+    this.checkboxes = [
+      ...document.querySelector(".load__vorratspunkte").children,
+    ];
 
-        this.checkboxes.forEach((checkbox, index) => {
-            checkbox.addEventListener('click', () => this.onCheckboxClicked(index));
-        });
-    }
+    this.checkboxes.forEach((checkbox, index) => {
+      checkbox.addEventListener("click", () => this.onCheckboxClicked(index));
+    });
+  }
 
-    onCheckboxClicked(clickedIndex) {
-        this.checkboxes.forEach((checkbox, index) => {
-            checkbox.checked = index <= clickedIndex;
-        });
-    }
+  onCheckboxClicked(clickedIndex) {
+    this.checkboxes.forEach((checkbox, index) => {
+      checkbox.checked = index <= clickedIndex;
+    });
+  }
 }
