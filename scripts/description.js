ffb1866f3a819de47b2d5e6cc93fcf4ae1fa1a7e
HillbrechtS <54956252+Paraldos@users.noreply.github.com>
2024-11-07 10:00:02 +0000

    complet reset

--- scripts/description.js
+++
@@ -1,9 +0,0 @@
-export default class Description {
-  constructor(text) {
-    this.container = document.querySelector(".left");
-    this.description = document.createElement("p");
-    this.description.classList.add("description");
-    this.description.innerHTML = `${text}`;
-    this.container.appendChild(this.description);
-  }
-}
--- scripts/playbook.js
+++
@@ -1,5 +0,0 @@
-import Krieger from "./playbooks/krieger.js";
-import Schatten from "./playbooks/schatten.js";
-
-const playbook = new Schatten();
-export default playbook;
