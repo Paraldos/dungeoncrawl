2617c585f4d545f93c83d0cee4ec96b713ae1650
Paraldos <hillbrecht.s@gmail.com>
2024-11-07 22:32:57 +0100

    added fonts and some card styling

--- components/Card.js
+++ components/Card.js
@@ -1,29 +1,36 @@
 export default class Card {
-    constructor(parent, config = {}) {
-        this.parent = parent;
-        this.headline = config.headline || '';
-        this.txt = config.txt || [];
-        this.card = this.createCard();
-        this.addHeadline();
-        this.addText();
-    }
+  constructor(parent, config = {}) {
+    this.parent = parent;
+    this.headline = config.headline || "";
+    this.cardType = config.cardType || "";
+    this.txt = config.txt || [];
+    this.card = this.createCard();
+    this.addHeadline();
+    this.addType();
+    this.addText();
+  }
 
-    createCard() {
-        const card = document.createElement('div');
-        card.className = 'card'
-        this.parent.appendChild(card);
-        return card
-    }
+  createCard() {
+    const card = document.createElement("div");
+    card.className = "card";
+    this.parent.appendChild(card);
+    return card;
+  }
 
-    addHeadline() {
-        if (!this.headline) return
-        this.card.innerHTML = `<h2 class="card__headline" >${this.headline}</h2>`;
-    }
+  addHeadline() {
+    if (!this.headline) return;
+    this.card.innerHTML = `<h2 class="card__headline" >${this.headline}</h2>`;
+  }
 
-    addText() {
-        if (!this.txt) return
-        this.txt.forEach(text => {
-            this.card.innerHTML += `<p class="card__txt">${text}</p>`;
-        });
-    }
+  addType() {
+    if (!this.cardType) return;
+    this.card.innerHTML += `<h3 class="card__type">${this.cardType}</h3>`;
+  }
+
+  addText() {
+    if (!this.txt) return;
+    this.txt.forEach((text) => {
+      this.card.innerHTML += `<p class="card__txt">${text}</p>`;
+    });
+  }
 }
--- main.js
+++ main.js
@@ -3,19 +3,46 @@ import Page from "./components/Page.js";
 import Card from "./components/Card.js";
 
 class main {
-    constructor() {
-        this.body = new Body();
-        this.body.cleanUp();
-        this.page = new Page();
+  constructor() {
+    this.body = new Body();
+    this.body.cleanUp();
+    this.page = new Page();
 
-        new Card(this.page.page, {headline: 'Test', txt: ['Test Text 1', 'Test Text 2']});
-        new Card(this.page.page);
-        new Card(this.page.page);
-        new Card(this.page.page);
-        new Card(this.page.page);
-        new Card(this.page.page);
-        new Card(this.page.page);
-
-    }
+    new Card(this.page.page, {
+      headline: "Schwert",
+      cardType: "Werkzeug",
+      txt: ["+W6 auf Nahkampf Attacken"],
+    });
+    new Card(this.page.page, {
+      headline: "Axt",
+      cardType: "Werkzeug",
+      txt: ["+W6 auf Nahkampf Attacken"],
+    });
+    new Card(this.page.page, {
+      headline: "Speer",
+      cardType: "Werkzeug",
+      txt: ["+W6 auf Nahkampf Attacken"],
+    });
+    new Card(this.page.page, {
+      headline: "Bogen",
+      cardType: "Werkzeug",
+      txt: ["+W6 auf Fernkampf Attacken"],
+    });
+    new Card(this.page.page, {
+      headline: "Armbrust",
+      cardType: "Werkzeug",
+      txt: ["+W6 auf Fernkampf Attacken"],
+    });
+    new Card(this.page.page, {
+      headline: "Lederrüstung",
+      cardType: "Werkzeug",
+      txt: ["+W6 auf Verteidigung"],
+    });
+    new Card(this.page.page, {
+      headline: "Kettenhemd",
+      cardType: "Werkzeug",
+      txt: ["+W6 auf Verteidigung"],
+    });
+  }
 }
 new main();
