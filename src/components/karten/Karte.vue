9d14142dad196ac6125bb770ffd391a2c3ceeb5d
Paraldos <hillbrecht.s@gmail.com>
2024-11-09 09:51:51 +0100

    added karten

---
+++ src/components/karten/Karte.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: Array,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="karte">
+    <h2>{{ props.title }}</h2>
+    <h3>{{ props.type }}</h3>
+    <ul>
+      <li v-for="desc in props.description" :key="desc">{{ desc }}</li>
+    </ul>
+  </div>
+</template>
+
+<style>
+.karte {
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  width: 5.9cm;
+  height: 9.1cm;
+  text-align: center;
+}
+
+h2 {
+  margin-bottom: 10px;
+  font-size: 1.2rem;
+}
+
+h3 {
+  margin-bottom: 10px;
+  font-size: 1rem;
+}
+
+ul {
+  list-style-type: none;
+}
+</style>
--- src/components/karten/Karten.vue
+++ src/components/karten/Karten.vue
@@ -1,7 +1,58 @@
-<script setup></script>
+<script setup>
+import Karte from "./Karte.vue";
+
+const carten = [
+  {
+    id: 1,
+    title: "Title",
+    type: "type",
+    description: ["Beschreibung 1", "Beschreibung 2"],
+  },
+  {
+    id: 2,
+    title: "Title",
+    type: "type",
+    description: ["Beschreibung 1", "Beschreibung 2"],
+  },
+  {
+    id: 3,
+    title: "Title",
+    type: "type",
+    description: ["Beschreibung 1", "Beschreibung 2"],
+  },
+  {
+    id: 4,
+    title: "Title",
+    type: "type",
+    description: ["Beschreibung 1", "Beschreibung 2"],
+  },
+  {
+    id: 5,
+    title: "Title",
+    type: "type",
+    description: ["Beschreibung 1", "Beschreibung 2"],
+  },
+];
+</script>
 
 <template>
-  <section class="">Test</section>
+  <section class="karten">
+    <Karte
+      v-for="karte in carten"
+      :key="karte.id"
+      :title="karte.title"
+      :type="karte.type"
+      :description="karte.description"
+    />
+  </section>
 </template>
 
-<style></style>
+<style>
+.karten {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  justify-content: center;
+  align-items: center;
+}
+</style>
