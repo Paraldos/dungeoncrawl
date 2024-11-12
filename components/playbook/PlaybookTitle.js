df1496dd326096a5626d553a2cf97362848ef5b1
Paraldos <hillbrecht.s@gmail.com>
2024-11-12 19:42:34 +0100

    refactored playbook title

--- components/playbook/PlaybookTitle.js
+++ components/playbook/PlaybookTitle.js
@@ -1,8 +1,9 @@
 export default class PlaybookTitle {
   getPlaybookTitle(title) {
     return `
-        <div class="playbook__playbook-title">
+        <div class="playbook__playbook-title blackbox">
             <h2>${title}</h2>
+            <p>Level</p>
             <input></input>
         </div>
     `;
