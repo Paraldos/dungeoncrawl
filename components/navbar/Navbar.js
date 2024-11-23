68e5c8cf63bb04e997f7374249648c813477bd22
Paraldos <hillbrecht.s@gmail.com>
2024-11-23 13:18:29 +0100

    added favicon

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -1,48 +1,11 @@
 export default class Navbar {
   constructor() {
     this.navbar = document.querySelector("nav");
-    this.navLinks = document.querySelectorAll(".navbar__link button");
-    this.pages = document.querySelectorAll(".page");
-    this.navLinks.forEach((link) => {
-      link.addEventListener("click", () => this.onClickNavLink(link));
-    });
-    this.submenuPlaybooks = document.querySelector(
-      ".navbar__submenu--playbooks"
-    );
-    document.addEventListener("playbookAdded", (e) =>
-      this.onPlaybookAdded(e.detail.txt, e.detail.id)
-    );
+    this.startBtn = document.querySelector(".start");
+    this.startBtn.addEventListener("click", () => this.onClickStart());
   }
 
-  onPlaybookAdded(txt, id) {
-    const link = document.createElement("li");
-    link.innerHTML = `<a class="button" href="#${id}">${txt}</a>`;
-    this.submenuPlaybooks.appendChild(link);
-  }
-
-  onClickNavLink(selectedLink) {
-    this.focusBtn(selectedLink);
-    this.focusPages(selectedLink);
-  }
-
-  focusBtn(selectedLink) {
-    this.navLinks.forEach((link) => {
-      link == selectedLink
-        ? link.classList.add("btn-selected")
-        : link.classList.remove("btn-selected");
-    });
-  }
-
-  focusPages(selectedLink) {
-    const target = selectedLink.getAttribute("data-target");
-    if (target) {
-      this.pages.forEach((page) => {
-        page.classList.contains(target)
-          ? page.classList.remove("hidden")
-          : page.classList.add("hidden");
-      });
-    } else {
-      this.pages.forEach((page) => page.classList.remove("hidden"));
-    }
+  onClickStart() {
+    window.scrollTo(0, 0);
   }
 }
--- main.js
+++ main.js
@@ -2,6 +2,6 @@ import Navbar from "./components/navbar/Navbar.js";
 import Regeln from "./components/rules/Rules.js";
 import playbooks from "./components/playbook/Playbooks.js";
 
-new Navbar();
+window.navbar = new Navbar();
 new Regeln();
 new playbooks();
