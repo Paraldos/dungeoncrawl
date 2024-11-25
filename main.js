ec95acff0313594edffc6c0b1a7afc8dfc27cc43
Paraldos <hillbrecht.s@gmail.com>
2024-11-25 19:09:08 +0100

    Added section

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ import Navbar from "/scripts/navbar/Navbar.js";
 import Regeln from "/scripts/rules/Rules.js";
 import Examples from "/scripts/rules/Examples.js";
 import playbooks from "/scripts/rules/Playbooks.js";
+import GroupSheet from "./scripts/rules/GroupSheet.js";
 
 window.navbar = new Navbar();
+new GroupSheet();
 new Regeln();
 new Examples();
 new playbooks();
--- scripts/rules/GroupSheet.js
+++ scripts/rules/GroupSheet.js
@@ -1,7 +1,11 @@
 import page from "../Page.js";
 
-export default class playbooks extends page {
+export default class GroupSheet extends page {
   constructor() {
     super();
+
+    const section = this.addSection("group-sheet");
+    section.classList.add("group-sheet");
+    window.navbar.addNavbarItem("Group Sheet", "group-sheet");
   }
 }
