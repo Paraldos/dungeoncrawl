44d4d1252d478a761c0569a0043988879af12621
Paraldos <hillbrecht.s@gmail.com>
2024-11-26 20:06:09 +0100

    added rules header and styling to group sheet

--- scripts/rules/GroupSheet.js
+++ scripts/rules/GroupSheet.js
@@ -3,9 +3,17 @@ import page from "../Page.js";
 export default class GroupSheet extends page {
   constructor() {
     super();
-
     const section = this.addSection("group-sheet");
-    section.classList.add("group-sheet");
+    section.classList.add("rules");
     window.navbar.addNavbarItem("Group Sheet", "group-sheet");
+
+    section.innerHTML += `<h2>Gruppenbogen ${this.svg.getD20()}</h2>`;
+    const gridContainer = this.addDiv(
+      "section__grid section__grid--3-colums",
+      section
+    );
+    const columnLeft = this.addFlexColumn(gridContainer);
+    const columnMiddle = this.addFlexColumn(gridContainer);
+    const columnRight = this.addFlexColumn(gridContainer);
   }
 }
