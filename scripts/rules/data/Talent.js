107aa6e3c038850a327ca6a6c8b5d3a7a4429735
Paraldos <hillbrecht.s@gmail.com>
2024-12-04 14:20:04 +0100

    replaced

--- scripts/rules/data/Talent.js
+++
@@ -1,7 +0,0 @@
-export default class Talent {
-  constructor(title = "", description = "", level = [false]) {
-    this.title = title;
-    this.description = description;
-    this.level = level;
-  }
-}
